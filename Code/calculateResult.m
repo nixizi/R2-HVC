218bd2b2752724675b973d1688ac1412330f1a93
nixizi <11510602@mail.sustc.edu.cn>
2018-07-30 10:47:47 +0800

    Add some comments

--- Code/calculateResult.m
+++ Code/calculateResult.m
@@ -1,17 +1,19 @@
 function result_set = calculateResult(data_set, num_vec)
-% Calculate HVC R2C newR2C
+% Calculate R2C and newR2C
 [data_size, dimension, data_set_size] = size(data_set);
-% result_set = zeros(5, data_size, data_set_size);
+% Initialize a matrix with dimentio
 result_set = zeros(4, data_size, data_set_size);
 
+% Iterate all data_set
 for k = 1:data_set_size
     result_set(:, :, k) = result_single(data_set(:, :, k), num_vec);
 end
 
     function result_one = result_single(data, num_vec)
-        % Calculate single HVC R2C newR2C result
+        % Calculate single R2C and newR2C result
         
-        %dimension
+        % dim: dimension
+        % Num: Number of point
         [Num,dim] = size(data);
 
         %generate vectors W
@@ -20,14 +22,13 @@ end
         W = W./sqrt(sum(W.^2,2));
 
         %Store results
-%         HVC = zeros(1,Num);
         R2C = zeros(2,Num);
         newR2C = zeros(2,Num);
         %Some constant values 
-%         HVvalue = HyperVolume(data*(-1));
         R2HV = R2ind(data,W,dim);
         R2 = R2ind(data,W,1);
 
+        % Iterate all points in a single data_set
         for i=1:Num
             data1 = data;
             s = data1(i,:);
@@ -36,23 +37,22 @@ end
             len = sqrt(sum(s.^2,2));
             lambda = s./len;
 
-            %Hypervolume contribution of solution s
-%             HVC(i) = HVvalue - HyperVolume(data1*(-1));
-
-            %R2 contribution by method 1
-%             R2C(1,i) = R2 - R2ind(data1,W,1);
+            %R2 contribution by old method
             R2C(1,i) = len - R2ind(data1,lambda,1);
             R2C(2,i) = R2HV - R2ind(data1,W,dim);
 
-            %R2 contribution by method 2
+            %R2 contribution by new method
             newR2C(1,i) = newR2ind(data1,W,1,s);
             newR2C(2,i) = newR2ind(data1,W,dim,s);
         
         end
-%         result_one = [HVC; R2C; newR2C];
         result_one = [R2C; newR2C];
         
             function R2val = newR2ind(data,W,dim,s)
+                % data: A data set
+                % W: Weight vector grid
+                % s: a data in the data set(data)
+                % Calculate R2 by new method
                 result = zeros(size(W));
                 for j=1:length(W)
                     temp = (s-data)./W(j,:);
@@ -66,6 +66,7 @@ end
             end
 
             function R2val = R2ind(data,W,dim)
+                % Calculate R2
                 result = zeros(size(W));
                 [row, line] = size(W);
                 for j=1:row
