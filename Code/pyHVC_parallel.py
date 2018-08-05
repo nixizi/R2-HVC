d306589549c7be9a7f1c5ab98fe9a7bc6a31c682
nixizi <11510602@mail.sustc.edu.cn>
2018-08-05 14:20:12 +0800

    Merge

--- Code/pyHVC_parallel.py
+++ Code/pyHVC_parallel.py
@@ -65,7 +65,7 @@ if __name__=="__main__":
     point_num = 100
     dimension = 15
     data_set_size = 100
-    data_type = "linear"
+    data_type = "random"
     data = read_data("data_set_{0}_{1}_{2}_{3}.mat".format(dimension, point_num, data_type, data_set_size), "data_set")
 
     # Enqueue Jobs
