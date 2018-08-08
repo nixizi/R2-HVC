8ef5b9ee62ec00b9ee73a05f6486e4a6b062c6a6
nixizi <11510602@mail.sustc.edu.cn>
2018-08-08 21:43:31 +0800

    Add new result

--- Code/generateAllDataSet.m
+++ Code/generateAllDataSet.m
@@ -3,8 +3,8 @@ solution_number = 100;
 set_number = 100;
 %dimension = 5;
 
-for problem_type = [string('convex_invertedtriangular'), string('concave_triangular')]
-% for problem_type = [string('convex_triangular'), string('linear_invertedtriangular')]
+%for problem_type = [string('convex_invertedtriangular'), string('concave_triangular')]
+for problem_type = [string('convex_triangular'), string('linear_invertedtriangular')]
 %for problem_type = [string('random')]
     % File name data_set_d_solutionNumber_problemType_setNum_numVector
     data_set_file_name = sprintf('data_set_%d_%d_%s_%d.mat', dimension, solution_number, problem_type, set_number);
@@ -23,7 +23,7 @@ for problem_type = [string('convex_invertedtriangular'), string('concave_triangu
         for seed = 1:30
             % File name result_set_d_solutionNumber_problemType_setNum_numVector
              result_set_file_name = sprintf('result_set_%d_%s_numVec_%d_seed_%d.mat', dimension, problem_type, num_vector, seed);
-             if exist(result_set_file_name) ~= 2
+             if exist(result_set_file_name, 'file') ~= 2
                 result_set = calculateResult(data_set, num_vector, seed);
                 result_set = [HVC; result_set];
                 parsave(result_set_file_name, result_set);
